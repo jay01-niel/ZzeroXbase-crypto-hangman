cfa8e95438cc48fae12b94c547f135f1cc67cfd7
jay01-niel <68505297+jay01-niel@users.noreply.github.com>
2023-10-08 23:12:38 +0100

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,83 @@
+const wordList = [
+  {
+    word: "bitcoin",
+    hint: "The first and most well-known cryptocurrency.",
+  },
+  {
+    word: "blockchain",
+    hint: "A decentralized, distributed ledger technology.",
+  },
+  {
+    word: "ethereum",
+    hint: "A blockchain platform with smart contract capabilities.",
+  },
+  {
+    word: "wallet",
+    hint: "Where you store your cryptocurrency holdings.",
+  },
+  {
+    word: "mining",
+    hint: "The process of validating transactions and adding them to the blockchain.",
+  },
+  {
+    word: "exchange",
+    hint: "A platform for buying, selling, or trading cryptocurrencies.",
+  },
+
+  {
+    word: "decentralized",
+    hint: "The absence of a central authority in a network.",
+  },
+  {
+    word: "token",
+    hint: "A unit of value issued by a blockchain project.",
+  },
+  {
+    word: "hodl",
+    hint: "A slang term meaning to hold onto your cryptocurrencies.",
+  },
+  {
+    word: "altcoin",
+    hint: "Any cryptocurrency other than Bitcoin.",
+  },
+  {
+    word: "smart contract",
+    hint: "Self-executing contracts with coded terms.",
+  },
+  {
+    word: "consensus",
+    hint: "The mechanism by which participants agree on the blockchain's state.",
+  },
+  {
+    word: "public key",
+    hint: "A cryptographic key for receiving cryptocurrency.",
+  },
+  {
+    word: "private key",
+    hint: "A secret key for accessing and controlling cryptocurrency.",
+  },
+  {
+    word: "wallet address",
+    hint: "A string for sending or receiving cryptocurrency.",
+  },
+  {
+    word: "cold storage",
+    hint: "A secure method of offline cryptocurrency storage.",
+  },
+  {
+    word: "cryptography",
+    hint: "The science of secure communication in cryptocurrencies.",
+  },
+  {
+    word: "decentralized finance",
+    hint: "Financial services built on blockchain without intermediaries.",
+  },
+  {
+    word: "liquidity",
+    hint: "The ease of converting assets into cash without affecting market prices.",
+  },
+  {
+    word: "fork",
+    hint: "A divergence in the blockchain's history, creating two separate chains.",
+  },
+];
